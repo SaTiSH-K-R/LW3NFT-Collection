6a006d8725a9e392cc6194cb65ac203561dd1067
SaTiSH-K-R <SaTiSH-K-R@users.noreply.github.com>
2022-06-24 14:33:27 +0530

    First Commit

---
+++ contract-dev/scripts/deploy.js
@@ -0,0 +1,25 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  const whitelistContract = "0x6BB6b7b4cE62Ae9e73ada7772bDCA9cFe39dA30e";
+  const metadataURL = "";
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  console.log(
+    "Crypto Devs Contract Address:",
+    deployedCryptoDevsContract.address
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
